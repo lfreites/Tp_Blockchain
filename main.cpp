5ef2783e9bac8cf78da4e5ec44aba489a07d9810
lucianojadur <ljadur@fi.uba.ar>
2020-11-09 20:44:47 -0300

    actualizo el main: ahora el archivo de salida se abre en modo append.

--- main.cpp
+++ main.cpp
@@ -11,24 +11,31 @@
 int main(int argc, char const *argv[]){
 	
 
-	ifstream aux;
-	aux.open("tnx.txt");
+	ifstream entrada;
+	entrada.open("tnx.txt");
 
 	transaccion_t *transaccion = transaccion_crear();
 
-	if (!transaccion_leer(transaccion, aux)){
-		aux.close();
+	if (!transaccion_leer(transaccion, entrada)){
+		entrada.close();
 		cerr << "error al leer" << endl;
+		transaccion_destruir(transaccion);
 		return -1;
 	}
 	transaccion_mostrar(transaccion);
 
 	ofstream salida;
-	salida.open("salida.txt");
+	salida.open("salida.txt", ios::app);
+	if(salida.fail()){
+		entrada.close();
+		cerr << "no se pudo abrir el archivo de salida" << endl;
+		transaccion_destruir(transaccion);
+		return -1;
+	}
 	transaccion_escribir(transaccion, salida);
 
 	salida.close();
-	aux.close();
+	entrada.close();
 
 	transaccion_destruir(transaccion);
 
