e745cda5a187ab0523659dfd32d930d13aea77d4
Luciano Jadur <66574334+lucianojadur@users.noreply.github.com>
2020-11-08 22:53:21 -0300

    Update input.h

--- input.h
+++ input.h
@@ -9,24 +9,24 @@ struct outpoint{
 
 typedef struct outpoint outpoint_t;
 
-
 class Input{	
 private:
 	outpoint_t outpoint; //Como el outpoint es algo propio del input, no abstraigo sus campos de la interfaz del input
 	hash_t addr;
-
 public:
 	Input(hash_t id, int index, hash_t adress);
 	~Input();
-	Input *obtenerInput(std::string data);
-	//devuelve un puntero a input a partir de de una cadena de texto (data) con los parámetros del input.
-	//En caso de la cadena ser errónea devuelve puntero a NULL.
-	//
-
+	
 	/*
 	getters
 	*/
 
 };
+
+Input *obtenerInput(std::string data);
+	//devuelve un puntero a input a partir de de una cadena de texto (data) con los parámetros del input.
+	//En caso de la cadena ser errónea devuelve puntero a NULL.
+	//
+
 
 #endif	//INPUT_H
