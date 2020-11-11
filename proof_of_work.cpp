54e79bdd0cf65571f5811e37c4f57eda4bf42a17
lucianojadur <ljadur@fi.uba.ar>
2020-11-11 16:53:33 -0300

    actualizo proof_of_work

--- proof_of_work.cpp
+++ proof_of_work.cpp
@@ -1,19 +1,34 @@
-map<char,uint> hexa = {
+#include "bloque.h"
+#include "utils.h"
 
-{'0', 4}, {'1', 3}, {'2', 2}, {'3', 2}, {'4', 1}, {'5', 1}, {'6', 1}, {'7', 1}, {'8', 0},
-	{'9', 0}, {'a', 0}, {'b', 0}, {'c', 0}, {'d', 0}, {'e', 0}, {'f', 0}
-};
+map<char,uint> hexa = { {'0', 4},
+						{'1', 3},
+						{'2', 2}, 
+						{'3', 2}, 
+						{'4', 1}, 
+						{'5', 1}, 
+						{'6', 1}, 
+						{'7', 1}, 
+						{'8', 0},
+						{'9', 0}, 
+						{'a', 0}, 
+						{'b', 0}, 
+						{'c', 0}, 
+						{'d', 0}, 
+						{'e', 0}, 
+						{'f', 0} };
 
-bool proof_of_work(header_t * header){
+
+bool proof_of_work(Bloque &bloque){
 
 	uint i;
-	string hash;
+	hash_t hash;
 	uint bits_nulos_hash = 0;
 
-	while(bits_nulos_hash < header -> bits) {
+	while(bits_nulos_hash < bloque.bits()) {
 
-		header -> nonce += 1;
-		hash = sha256(sha256((header->prev_block)+'\n'+(header->txns_hash)+'\n'+(to_string((header->bits)))+'\n'+(to_string(header->nonce))+'\n'));
+		bloque -> nonce += 1;
+		hash = sha256(sha256((bloque.prev_block())+'\n'+(bloque.txns_hash())+'\n'+(to_string((bloque.bits())))+'\n'+(to_string(bloque->nonce()))+'\n'));
 		bits_nulos_hash = 0;
 	
 		for(i = 0; hash[i] == '0' ; i++) {
